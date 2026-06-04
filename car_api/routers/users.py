1f1c9f56066ae06ff01a65871e19b26391b1b4b9
Carlos Arruda <caugustogarruda@gmail.com>
2026-06-04 10:17:53 -0300

    Adding response model to list users

--- car_api/routers/users.py
+++ car_api/routers/users.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, status
-
+from car_api.schemas.users import UserListPublicSchema
 
 router = APIRouter()
 
 
-@router.get('/', status_code=status.HTTP_200_OK)
+@router.get(path='/', status_code=status.HTTP_200_OK, response_model=UserListPublicSchema)
 async def list_users():
     return {
         'users': [
-            {'id': 1, 'email': 'carlos@gmail.com'},
-            {'id': 2, 'email': 'carlos1@gmail.com'},
-            {'id': 3, 'email': 'carlos2@gmail.com'},
+            {'id': 1, 'username': 'Carlos', 'email': 'carlos@gmail.com'},
+            {'id': 2, 'username': 'Augusto', 'email': 'carlos1@gmail.com'},
+            {'id': 3, 'username': 'Arruda', 'email': 'carlos2@gmail.com'},
         ]
     }
