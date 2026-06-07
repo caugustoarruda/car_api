cb96f15dcdf8e97706a3efbb952cc42546478f1d
Carlos Arruda <caugustogarruda@gmail.com>
2026-06-07 09:04:33 -0300

    Model cars part 1

--- car_api/models/cars.py
+++ car_api/models/cars.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 from typing import Optional
 from sqlalchemy import ForeignKey, String, Text, Numeric, Integer, func
 from sqlalchemy.orm import Mapped,  mapped_column, relationship
@@ -7,12 +8,35 @@ from car_api.models import Base
 
 
 class Brand(Base):
-    __tablename__='users'
+    __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), unique=True)
     is_active: Mapped[bool] = mapped_column(default=True)
     description: Mapped[Optional[str]] = mapped_column(Text, default=None)
+
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        onupdate=func.now(), server_default=func.now()
+    )
+
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    model: Mapped[str] = mapped_column(String(100))
+    factory_year: Mapped[int] = mapped_column(Integer)
+    model_year: Mapped[int] = mapped_column(Integer)
+    color: Mapped[str] = mapped_column(String(30))
+    plate: Mapped[str] = mapped_column(String(10), unique=True, index=True)
+
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    description: Mapped[Optional[str]] = mapped_column(Text, default=None)
+    is_available: Mapped[bool] = mapped_column(default=True)
 
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
