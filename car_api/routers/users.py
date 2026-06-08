5963ceb6b54a3dd64a546e59f627efe81587efbd
Carlos Arruda <caugustogarruda@gmail.com>
2026-06-08 13:51:14 -0300

    Username validation

--- car_api/routers/users.py
+++ car_api/routers/users.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, exists
 from car_api.schemas.users import UserListPublicSchema, UserSchema, UserPublicSchema
 from car_api.core.database import get_session
 from car_api.db import USERS
@@ -11,6 +12,14 @@ router = APIRouter()
 
 @router.post(path='/', status_code=status.HTTP_201_CREATED, response_model=UserPublicSchema, summary='Criar novo usuário')
 async def create_user(user: UserSchema, db: AsyncSession = Depends(get_session)):
+    username_exists = await db.scalar(select(exists().where(User.username == user.username)))
+
+    if username_exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Username já esta sendo utilizado.'
+        )
+    
     db_user = User(
         username=user.username,
         email=user.email,
