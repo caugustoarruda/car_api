6983bddb29a7c2c9cdc4e4878edf7f0c1ee32c34
Carlos Arruda <caugustogarruda@gmail.com>
2026-06-20 10:25:01 -0300

    Username and password schema validator

--- car_api/schemas/users.py
+++ car_api/schemas/users.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-from pydantic import BaseModel, EmailStr, ConfigDict
+from pydantic import BaseModel, EmailStr, ConfigDict, field_validator
 from datetime import datetime
 
 
@@ -8,10 +8,22 @@ class UserSchema(BaseModel):
     email: EmailStr
     password: str
 
+    @field_validator('username')
+    def username_min_length(cls, field):
+        if len(field) < 3:
+            raise ValueError('Username deve ter pelo menos 3 caracteres')
+        return field
+    
+    @field_validator('password')
+    def password_min_length(cls, field):
+        if len(field) < 3:
+            raise ValueError('Password deve ter pelo menos 3 caracteres')
+        return field
+
 
 class UserPublicSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    
+
     id: int
     username: str
     email: EmailStr
@@ -24,6 +36,18 @@ class UserUpdateSchema(BaseModel):
     username: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
+
+    @field_validator('username')
+    def username_min_length(cls, field):
+        if len(field) < 3:
+            raise ValueError('Username deve ter pelo menos 3 caracteres')
+        return field
+    
+    @field_validator('password')
+    def password_min_length(cls, field):
+        if len(field) < 3:
+            raise ValueError('Password deve ter pelo menos 3 caracteres')
+        return field
 
 
 class UserListPublicSchema(BaseModel):
